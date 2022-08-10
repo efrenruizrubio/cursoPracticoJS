e02dd32a4cf1ca1ed3fe4d83e76ba30450ab969a
efrenruizrubio <efren282@outlook.es>
2022-08-10 17:33:52 -0500

    :sparkles: [ADD]: Basic DOM manipulation

---
+++ domManipulation.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const paragraph = document.getElementsByClassName(".paragraph");
+const paragraphID = document.getElementById("#paragraph-id");
+const input = document.querySelector("input");
+
+console.log(input.value);
+
+console.log({ h1, p, paragraph, paragraphID, input });
