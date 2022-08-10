7593b08bc1149ce80748cdfb3bd2b8c0b4fc4a5e
efrenruizrubio <efren282@outlook.es>
2022-08-10 17:49:45 -0500

    :sparkles: [ADD]: More basic manipulation of the DOM

--- domManipulation.js
+++ domManipulation.js
@@ -1,9 +1,29 @@
 const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
-const paragraph = document.getElementsByClassName(".paragraph");
-const paragraphID = document.getElementById("#paragraph-id");
+const paragraph = document.getElementsByClassName("paragraph");
+const paragraphID = document.getElementById("paragraph-id");
 const input = document.querySelector("input");
 
 console.log(input.value);
 
 console.log({ h1, p, paragraph, paragraphID, input });
+
+h1.innerHTML = "Hello World <br/> good";
+h1.innerText = "Hello World <br/> good";
+/* console.log(h1.getAttribute("class"));
+h1.setAttribute("class", "red");
+console.log(h1.getAttribute("class")); */
+
+h1.classList.add("red");
+h1.classList.remove("blue");
+//h1.classList.toggle("blue");
+//h1.classList.contains("blue");
+
+input.value = "456";
+
+const img = document.createElement("img");
+img.setAttribute("src", "https://picsum.photos/200");
+
+paragraphID.innerHTML = "";
+paragraphID.append(img);
+/* paragraphID.replaceWith(img); */
